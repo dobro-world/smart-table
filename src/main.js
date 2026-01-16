25522dba971aacb01aa3c6823f66a4779586c643
Aleksandr Arestov <dobro.world@yandex.ru>
2026-01-16 14:32:09 +0700

    Added: pagintation, data in tables

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
 // @todo: подключение
 
+import {initPagination} from "./components/pagination.js";
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -19,9 +20,13 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
 
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -34,6 +39,7 @@ function render(action) {
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
 
+    result = applyPagination(result, state, action);
 
     sampleTable.render(result)
 }
@@ -42,10 +48,22 @@ const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
     before: [],
-    after: []
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
+    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
 
 const appRoot = document.querySelector('#app');

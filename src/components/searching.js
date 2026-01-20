820b3a3de9e6a6e14a9921fe008a9cd8c29e81f2
Aleksandr Arestov <dobro.world@yandex.ru>
2026-01-20 18:02:06 +0700

    Added: Searching

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,17 @@
 import {rules, createComparison} from "../lib/compare.js";
 
-
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-
+    const skipEmptyRule = rules.skipEmptyTargetValues();
+    
+    const searchRule = rules.searchMultipleFields(
+        searchField,
+        ['date', 'customer', 'seller'],
+        false
+    );
+    
+    const compare = createComparison([], [skipEmptyRule, searchRule]);
+    
     return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
-    }
+        return data.filter(row => compare(row, state));
+    };
 }
